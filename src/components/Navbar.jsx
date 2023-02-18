bf247ff3067e4d3aca5fabaa17da8830255c9d23
Ben Vilakazi <xolanithegeengubane@gmail.com>
2023-02-18 09:54:54 +0200

    new

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { SearchBar } from './';
+import { logo } from '../utils/constants';
+
+const  Navbar = () => (
+    <Stack direction="row" alignment="center" p={2} sx={{ postition: 'sticky', background: "#000", top: 0, justifyContent: 'space-between'}} >
+    <Link to="/" style={{display: "flex", alignItems: "center", }}>
+     <img src={logo} alt="logo" height={45} /> 
+    </Link>
+    <SearchBar />
+    </Stack>
+  );
+
+
+export default Navbar
